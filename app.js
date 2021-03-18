78bf98253bdf3ba5c85c5e5ccea33c505363fe59
harry <harry.makins@gmail.com>
2021-03-18 16:06:39 +0000

    updated code

--- app.js
+++ app.js
@@ -1,30 +1,126 @@
-function add (a, b) {
-	return a + b;
+class Calculator {
+    constructor(previousOperandDiv, currentOperandDiv) {
+    this.previousOperandDiv = previousOperandDiv;
+    this.currentOperandDiv = currentOperandDiv;
+    this.clear();
+    };
+
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
+    };
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    };
+
+    addNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    };
+
+    addOperator(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand != '') {
+            this.calculate();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    };
+
+    calculate() {
+        let calculation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                calculation = prev + current;
+                break
+            case '-':
+                calcultaion = prev - current;
+                break
+            case 'x':
+                calculation = prev * current;
+                break
+            case '÷':
+                calculation = prev / current;
+                break
+            default :
+            return;
+        }
+        this.currentOperand = calculation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    };
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        };
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        };
+    }
+
+    updateDisplay() {
+        this.currentOperandDiv.innerText = this.getDisplayNumber(this.currentOperand);
+        this.previousOperandDiv.innerText = this.previousOperand
+        if (this.operation != null) {
+            this.previousOperandDiv.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandDiv.innerText = '';
+        }
+    };
 }
 
-function subtract (a, b) {
-	return a - b;
-}
+const previousOperandDiv = document.querySelector('[data-previous-operand');
+const currentOperandDiv = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
 
-function multiply (a, b) {
-    return a * b;
-}
+const calculator = new Calculator(previousOperandDiv, currentOperandDiv);
 
-function divide (a, b) {
-    return a / b;
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
 
-function operate (num1, operator, num2) {
-    let result = 0;
-    if (operator == '+') {
-        result = add(num1, num2);
-    } else if (operator == '-') {
-        result = subtract(num1, num2);
-    } else if (operator == '*') {
-        result = multiply(num1, num2);
-    } else if (operator == '/') {
-        result = divide(num1, num2);
-    } else return;
-    return result;
-}
-console.log(operate(10, '*', 5));
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.addOperator(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.calculate();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
